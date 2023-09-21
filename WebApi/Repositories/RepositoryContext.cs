de224e3eff3ef72a640ec173241271ca7a2af94e
Nesimi <hn.nesimioglu@forestall.io>
2023-09-21 17:09:10 +0300

    repositoryContext ,connectionString, program.cs con string.
    RepositoryContext inherited DbContext
    Connection string defined in appsettings.json
    connectionstring has been added in program.cs

--- WebApi/Repositories/RepositoryContext.cs
+++ WebApi/Repositories/RepositoryContext.cs
@@ -5,6 +5,10 @@ namespace WebApi.Repositories
 {
     public class RepositoryContext : DbContext
     {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public DbSet<Book> Books { get; set; }
     }
 }
