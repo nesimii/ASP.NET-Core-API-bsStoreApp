e8f9f4dad37f914d929d7891eccb392ba051a2a6
Nesimi <hn.nesimioglu@forestall.io>
2023-09-21 23:28:24 +0300

    RepositoryManager added.

---
+++ Repositories/EFCore/RepositoryManager.cs
@@ -0,0 +1,23 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public abstract class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+
+        public RepositoryManager(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IBookRepository Book => new BookRepository(_context);
+
+        public void Save() => _context.SaveChanges();
+    }
+}
