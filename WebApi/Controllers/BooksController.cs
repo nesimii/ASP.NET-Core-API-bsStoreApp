02dea16e283eb32b4b7e9a6078a971d4f7659624
Nesimi <hn.nesimioglu@forestall.io>
2023-09-21 20:42:23 +0300

    IoC for RepositoryContext, BooksController created

---
+++ WebApi/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Repositories;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _context.Books.ToList();
+            return Ok(books);
+        }
+    }
+}
