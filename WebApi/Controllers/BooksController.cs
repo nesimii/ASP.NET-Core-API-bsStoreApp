3f192adb0f179c52e9a790fb6a533891280ebe3e
Nesimi <hn.nesimioglu@forestall.io>
2023-09-21 21:51:56 +0300

    getOne, update, put,delete, patch requests added.

--- WebApi/Controllers/BooksController.cs
+++ WebApi/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
 using WebApi.Repositories;
 
 namespace WebApi.Controllers
@@ -18,8 +20,105 @@ namespace WebApi.Controllers
         [HttpGet]
         public IActionResult GetAllBooks()
         {
-            var books = _context.Books.ToList();
-            return Ok(books);
+            try
+            {
+                var books = _context.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook(int id)
+        {
+            try
+            {
+                var book = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+                if (book == null) return NotFound();
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book == null) return BadRequest();
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        {
+            try
+            {
+                if (id != book.Id) return BadRequest();
+
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+                if (entity == null) return NotFound();
+
+                entity.Title = book.Title;
+                entity.Price = book.Price;
+                _context.SaveChanges();
+
+                return Ok(entity);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+                if (entity == null) return NotFound(new
+                {
+                    StatusCode = 404,
+                    message = $"Book with id:{id} could not found."
+                });
+                _context.Books.Remove(entity);
+                _context.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpPatch("{id:int}")]
+        public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody]
+        JsonPatchDocument<Book> bookPatch)
+        {
+            try
+            {
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+                if (entity == null) return NotFound();
+
+                bookPatch.ApplyTo(entity);
+                _context.SaveChanges();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
