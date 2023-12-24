d4f7eef1fddc15b5255e05ba082987cfb90aa73e
Nesimi <hasanesimi01@gmail.com>
2023-12-25 01:26:11 +0300

    global exception management

--- Presentation/Controllers/BooksController.cs
+++ Presentation/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts;
@@ -24,92 +25,49 @@ namespace Presentation.Controllers
         [HttpGet]
         public IActionResult GetAllBooks()
         {
-            try
-            {
-                var books = _manager.BookService.GetAllBooks(false);
-                return Ok(books);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            var books = _manager.BookService.GetAllBooks(false);
+            return Ok(books);
 
         }
         [HttpGet("{id:int}")]
         public IActionResult GetOneBook(int id)
         {
-            try
-            {
-                var book = _manager.BookService.GetOneBookById(id, false);
-                if (book == null) return NotFound();
-                return Ok(book);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            var book = _manager.BookService.GetOneBookById(id, false);
+            return Ok(book);
+
         }
         [HttpPost]
         public IActionResult CreateOneBook([FromBody] Book book)
         {
-            try
-            {
-                if (book == null) return BadRequest();
-                _manager.BookService.CreateOneBook(book);
-                return StatusCode(201, book);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            if (book == null) return BadRequest();
+            _manager.BookService.CreateOneBook(book);
+            return StatusCode(201, book);
         }
         [HttpPut("{id:int}")]
         public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
         {
-            try
-            {
-                if (id != book.Id || book == null) return BadRequest();
-
-                _manager.BookService.UpdateOneBook(id, book, true);
-
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            if (id != book.Id || book == null) return BadRequest();
+            _manager.BookService.UpdateOneBook(id, book, true);
+            return NoContent();
         }
         [HttpDelete("{id:int}")]
         public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
         {
-            try
-            {
-                _manager.BookService.DeleteOneBook(id, false);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            _manager.BookService.DeleteOneBook(id, false);
+            return NoContent();
+
         }
         [HttpPatch("{id:int}")]
         public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody]
         JsonPatchDocument<Book> bookPatch)
         {
-            try
-            {
-                var entity = _manager.BookService.GetOneBookById(id, true);
-                if (entity == null) return NotFound();
+            var entity = _manager.BookService.GetOneBookById(id, true);
 
-                bookPatch.ApplyTo(entity);
-                _manager.BookService.UpdateOneBook(id, entity, true);
+            bookPatch.ApplyTo(entity);
+            _manager.BookService.UpdateOneBook(id, entity, true);
 
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            return NoContent();
+
         }
     }
 }
--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Repositories.Contracts;
 using Services.Contracts;
@@ -32,12 +33,7 @@ namespace Services
         {
             //check entity
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
-            if (entity is null)
-            {
-                string message = $"The book with id:{id} could not found.";
-                _logger.LogInfo(message);
-                throw new Exception(message);
-            }
+            if (entity is null) throw new BookNotFoundException(id);
             _manager.Book.DeleteOneBook(entity);
             _manager.Save();
 
@@ -50,7 +46,9 @@ namespace Services
 
         public Book GetOneBookById(int id, bool trackChanges)
         {
-            return _manager.Book.GetOneBookById(id, trackChanges);
+            var book = _manager.Book.GetOneBookById(id, trackChanges);
+            if (book is null) throw new BookNotFoundException(id);
+            return book;
         }
 
         public void UpdateOneBook(int id, Book book, bool trackChanges)
@@ -59,12 +57,7 @@ namespace Services
 
             //check entity
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
-            if (entity is null)
-            {
-                string message = $"The book with id:{id} could not found.";
-                _logger.LogInfo(message);
-                throw new Exception(message);
-            }
+            if (entity is null) throw new BookNotFoundException(id);
 
             entity.Title = book.Title;
             entity.Price = book.Price;
