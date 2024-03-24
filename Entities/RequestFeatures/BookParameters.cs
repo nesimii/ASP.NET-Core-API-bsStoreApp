e2afe309d896e161ca2a8084552a719b5e180ae3
Nesimi <hasanesimi01@gmail.com>
2024-03-24 20:42:37 +0300

    title search

--- Entities/RequestFeatures/BookParameters.cs
+++ Entities/RequestFeatures/BookParameters.cs
@@ -5,5 +5,5 @@ public class BookParameters : RequestParameters
     public uint MinPrice { get; set; }
     public uint MaxPrice { get; set; } = 1000;
     public bool ValidPriceRange => MaxPrice > MinPrice;
-
+    public string? SearchTerm { get; set; }
 }
--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace Repositories.EFCore;
+﻿namespace Repositories.EFCore.Extensions;
 
 using Entities.Models;
 using Entities.RequestFeatures;
@@ -9,5 +9,13 @@ public static class BookRepositoryExtensions
     {
         return books.OrderBy(x => x.Id)
             .Where(book => book.Price >= bookParameters.MinPrice && book.Price <= bookParameters.MaxPrice);
+    }
+
+    public static IQueryable<Book> Search(this IQueryable<Book> books, string searchTerm)
+    {
+        if (string.IsNullOrWhiteSpace(searchTerm)) return books;
+
+        var lowerCaseTerm = searchTerm.Trim().ToLower();
+        return books.Where(b => b.Title.ToLower().Contains(lowerCaseTerm));
     }
 }
