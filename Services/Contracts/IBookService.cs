1f4264364000b7d6757fc7ffeb6075d936f08d49
Nesimi <hasanesimi01@gmail.com>
2024-10-08 23:40:55 +0300

    hateoas has been added.

--- Services/Contracts/IBookService.cs
+++ Services/Contracts/IBookService.cs
@@ -1,23 +1,17 @@
 ﻿using Entities.DataTransferObjects;
+using Entities.LinkModels;
 using Entities.Models;
 using Entities.RequestFeatures;
-using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Services.Contracts
+namespace Services.Contracts;
+
+public interface IBookService
 {
-    public interface IBookService
-    {
-        Task<(IEnumerable<ExpandoObject> books, MetaData metaData)> GetAllBooksWithFilterAsync(BookParameters bookParameters, bool trackChanges);
-        Task<BookDto> GetOneBookByIdAsync(int id, bool trackChanges);
-        Task<BookDto> CreateOneBookAsync(BookDtoForInsertion book);
-        Task UpdateOneBookAsync(int id, BookDtoForUpdate book, bool trackChanges);
-        Task DeleteOneBookAsync(int id, bool trackChanges);
-        Task<(BookDtoForUpdate bookDtoForUpdate, Book book)> GetOneBookForPatchAsync(int id, bool trackChanges);
-        Task SaveChangesForPatchAsync(BookDtoForUpdate bookDtoForUpdate, Book book);
-    }
+    Task<(LinkResponse linkResponse, MetaData metaData)> GetAllBooksWithFilterAsync(LinkParameters linkParameters, bool trackChanges);
+    Task<BookDto> GetOneBookByIdAsync(int id, bool trackChanges);
+    Task<BookDto> CreateOneBookAsync(BookDtoForInsertion book);
+    Task UpdateOneBookAsync(int id, BookDtoForUpdate book, bool trackChanges);
+    Task DeleteOneBookAsync(int id, bool trackChanges);
+    Task<(BookDtoForUpdate bookDtoForUpdate, Book book)> GetOneBookForPatchAsync(int id, bool trackChanges);
+    Task SaveChangesForPatchAsync(BookDtoForUpdate bookDtoForUpdate, Book book);
 }
