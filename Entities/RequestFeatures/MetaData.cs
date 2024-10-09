1c42cf9d5e44b83593250f64c6f0f486f32639cc
Nesimi <hasanesimi01@gmail.com>
2024-10-09 22:28:11 +0300

    options&head has been added

--- Entities/RequestFeatures/MetaData.cs
+++ Entities/RequestFeatures/MetaData.cs
@@ -6,6 +6,6 @@ public class MetaData
     public int TotalPage { get; set; }
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
-    public bool HasPrevius => CurrentPage > 1;
+    public bool HasPrevious => CurrentPage > 1;
     public bool HasNext => CurrentPage < TotalPage;
 }
